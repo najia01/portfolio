a1b9c6f606ccbee236a1a20097a01944fc1490c7
najia01 <validress@outlook.fr>
2023-05-03 16:07:55 +0200

    modif

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 
-// *********************style de la barre de navigation avec toogle menu*********************************
-
-
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-
-
-// const mobileMenu = () => {
-//   hamburger.classList.toggle("active")
-//   navMenu.classList.toggle("active")
-// }
-
-// const closeMenu = () => {
-//   hamburger.classList.remove("active")
-//   navMenu.classList.remove("active")
-// }
-
-// hamburger.addEventListener("click" , mobileMenu)
+// ***************************************************STYLE MENU BURGER AVEC TOOGLE MENU *******************************************************************
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -26,13 +9,15 @@ hamburger.addEventListener("click", () => {
 });
 
 
+// **********************************************STYLE DU TITRE H1 AVEC SAISIE TEXTE AUTOMATIQUE**************************************************************
 
-// **************************STYLE DU TITRE H1 AVEC SAISIE TEXTE AUTOMATIQUE**********************
-// *******************déclaration de la classe concernée ****************
+// *******************************************Déclaration de la classe concernée ***********************************************************************
 
 let typed = new Typed(".autochange",{
+  
     // ***Saisie des chaines de caractères que l'on veut saisir chacun entre ""**************
     strings: ["Développeuse Web","Programmeuse","Codeuse"],
+
     // **************Définition de la vitesse de saisie ************************************
     typeSpeed: 150,
     backSpeed: 150,
